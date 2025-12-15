a620377993425769dbaea2628d223b22be3e3b2b
xlinayo <xeila2012@gmail.com>
2025-12-15 19:17:36 +0100

    XEILA LIÑAYO Update ej3a1_operations.py

--- 3a/ej3a1/ej3a1_operations.py
+++ 3a/ej3a1/ej3a1_operations.py
@@ -3,19 +3,23 @@
 
 def add(num_1: float, num_2: float) -> float:
     # Write here your code
+    return num_1 + num_2
     pass
 
 
 def subtract(num_1: float, num_2: float) -> float:
     # Write here your code
+    return num_1 - num_2
     pass
 
 
 def multiply(num_1: float, num_2: float) -> float:
     # Write here your code
+    return num_1 * num_2
     pass
 
 
 def divide(num_1: float, num_2: float) -> float:
     # Write here your code
+    return num_1 / num_2
     pass
